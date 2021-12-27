8d1b5480fc751a918864d0c7fd760eb9ad54177d
Willow Hayward <WillHayCode@gmail.com>
2021-12-27 12:34:04 +1000

    feat(ui): implemented counter

--- libs/ui/src/lib/counter/counter.component.ts
+++ libs/ui/src/lib/counter/counter.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Input, Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'whc-counter',
@@ -6,10 +6,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./counter.component.scss']
 })
 export class CounterComponent implements OnInit {
+    @Input() value = 0;
+    @Input() step = 1;
+    @Input() min = 0;
+    @Input() max = 10;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  decrement(): void {
+      const newValue = this.value - this.step;
+      if (newValue >= this.min) {
+        this.value = newValue;
+      } else {
+          this.value = this.min;
+      }
+  }
+
+  increment(): void {
+      const newValue = this.value + this.step;
+      if (newValue <= this.max) {
+        this.value = newValue;
+      } else {
+          this.value = this.max;
+      }
   }
 
 }
