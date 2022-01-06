e7f02ccf4b6e654189bdbfb03ca8d88497e9d3c2
Willow Hayward <WillHayCode@gmail.com>
2022-01-06 13:35:30 +1000

    build(ridcully, ships): initialised projects

---
+++ apps/ridcully/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { UiModule } from '@willhaycode/ui';
+import { RouterModule } from '@angular/router';
+import { LandingComponent } from './landing/landing.component';
+
+@NgModule({
+  declarations: [AppComponent, LandingComponent],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot([
+
+    ], { initialNavigation: 'enabledBlocking' }),
+    UiModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
---
+++ apps/ships/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { UiModule } from '@willhaycode/ui';
+import { RouterModule } from '@angular/router';
+import { LandingComponent } from './landing/landing.component';
+import { GameComponent } from './game/game.component';
+
+@NgModule({
+  declarations: [AppComponent, LandingComponent, GameComponent],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot([
+        {
+            path: '',
+            //component: LandingComponent
+            component: GameComponent
+        },
+        {
+            path: 'play',
+            component: GameComponent
+        },
+    ], { initialNavigation: 'enabledBlocking' }),
+    UiModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
