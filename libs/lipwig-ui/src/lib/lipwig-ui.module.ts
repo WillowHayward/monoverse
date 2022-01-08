802538e79ac8b80fd6aee285de006c09f20629c0
Willow Hayward <WillHayCode@gmail.com>
2022-01-08 19:38:41 +1000

    feat(lipwig-ui): added landing page component

--- libs/lipwig-ui/src/lib/lipwig-ui.module.ts
+++ libs/lipwig-ui/src/lib/lipwig-ui.module.ts
@@ -2,11 +2,17 @@ import { NgModule } from '@angular/core';
 import { UiModule } from '@willhaycode/ui';
 import { CommonModule } from '@angular/common';
 import { CreateCardComponent } from './create-card/create-card.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
 @NgModule({
   imports: [CommonModule, UiModule],
   declarations: [
-    CreateCardComponent
+    CreateCardComponent,
+    LandingPageComponent
   ],
 })
 export class LipwigUiModule {}
+
+export {
+    LandingPageComponent
+}
