c7b1be5ac73ffeda3e39c1d749013baf376b61f5
Willow Hayward <WillHayCode@gmail.com>
2022-01-08 20:13:26 +1000

    feat(lipwig-ui): created player details and join cards

--- libs/lipwig-ui/src/lib/lipwig-ui.module.ts
+++ libs/lipwig-ui/src/lib/lipwig-ui.module.ts
@@ -3,12 +3,16 @@ import { UiModule } from '@willhaycode/ui';
 import { CommonModule } from '@angular/common';
 import { CreateCardComponent } from './create-card/create-card.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
+import { JoinCardComponent } from './join-card/join-card.component';
+import { DetailsCardComponent } from './details-card/details-card.component';
 
 @NgModule({
   imports: [CommonModule, UiModule],
   declarations: [
     CreateCardComponent,
-    LandingPageComponent
+    LandingPageComponent,
+    JoinCardComponent,
+    DetailsCardComponent
   ],
 })
 export class LipwigUiModule {}
