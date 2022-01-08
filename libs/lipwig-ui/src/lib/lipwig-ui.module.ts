2a528b3fd1fdd7c1e72042236eb8a8b8f0d1c650
Willow Hayward <WillHayCode@gmail.com>
2022-01-08 19:16:16 +1000

    feat(lipwig-ui): added room creation card

--- libs/lipwig-ui/src/lib/lipwig-ui.module.ts
+++ libs/lipwig-ui/src/lib/lipwig-ui.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
+import { UiModule } from '@willhaycode/ui';
 import { CommonModule } from '@angular/common';
+import { CreateCardComponent } from './create-card/create-card.component';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, UiModule],
+  declarations: [
+    CreateCardComponent
+  ],
 })
 export class LipwigUiModule {}
