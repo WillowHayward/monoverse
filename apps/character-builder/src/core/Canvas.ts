6240c9f3df7c38c03df86aa144bf2a45a28283f2
Willow Hayward <WillHayCode@gmail.com>
2022-01-16 21:53:56 +1000

    build(character-builder): implemented template vars

--- apps/character-builder/src/core/Canvas.ts
+++ apps/character-builder/src/core/Canvas.ts
@@ -1,4 +1,4 @@
-import { Loader, SpriteData } from './Loader';
+import { Loader, PreloadData } from './Loader';
 export const SPRITE_SCALE = 0.3;
 export class Canvas {
 
@@ -10,20 +10,35 @@ export class Canvas {
 
         ctx.fillStyle = '#89CFF0';
         ctx.fillRect(0, 0, canvas.width, canvas.height);
+        const templateVars: {[key:string]: string} = {}
 
-        Loader.getImage('tint1_head.png').then(head => {
-            head.draw(ctx, 0, 0, SPRITE_SCALE);
-        });
+        Loader.getJSON('preload.json').then((preload: PreloadData) => {
+            console.log("beginning preload", preload);
+            return Loader.preload(preload);
+        }).then(() => {
+            console.log("preloaded!", Loader.getCache());
+            return Loader.getJSON('default.json');
+        }).then(defaults => {
+            for (const label in defaults) {
+                templateVars[label] = defaults[label];
+            }
 
-        Loader.getJSON('spritesheets.json').then((spritesheets: any) => {
-            console.log(spritesheets);
-            for (const path in spritesheets) {
-                const spritesheet = spritesheets[path];
-                Loader.getSheet(path, spritesheet).then(sprites => {
-                    for (const sprite of sprites) {
-                        console.log(sprite);
-                        sprite.draw(ctx, 0, 0, SPRITE_SCALE);
+            return Loader.getJSON('parts.json');
+        }).then(parts => {
+            for (const part of parts) {
+                const type = part.type;
+                let path = part.path;
+                const x = part.x ?? 0;
+                const y = part.y ?? 0;
+                for (const templateVar in templateVars) {
+                    const matchString = `[${templateVar}]`;
+                    if (path.includes(matchString)) {
+                        const templateVarValue = templateVars[templateVar];
+                        path = path.replaceAll(matchString, templateVarValue);
                     }
+                }
+                Loader.getSprite(path).then(sprite => {
+                    sprite.draw(ctx, x, y, SPRITE_SCALE);
                 });
             }
         });
