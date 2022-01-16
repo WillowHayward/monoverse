b6f095e2647ce033140b941c543bf80d500c0a17
Willow Hayward <WillHayCode@gmail.com>
2022-01-16 19:25:50 +1000

    feat(character-builder): implement file loader and sprite classes

--- apps/character-builder/src/core/Loader.ts
+++ apps/character-builder/src/core/Loader.ts
@@ -1,4 +1,73 @@
+import { Sprite } from './Sprite';
 
+export type SpriteData = {
+    name: string,
+    x: number,
+    y: number,
+    width: number,
+    height: number
+}
+
+type SpriteMap = {[path: string]: Sprite};
 export class Loader {
-    
+    private static instance: Loader | null;
+    private sprites: SpriteMap = {};
+    private constructor() {
+        Loader.instance = this;
+    }
+
+    private static getInstance(): Loader {
+        if (Loader.instance) {
+            return Loader.instance;
+        }
+
+        return new Loader();
+    }
+
+    public static async getJSON(path: string): Promise<any> {
+        const loader = Loader.getInstance();
+        return loader.loadJSON(path);
+    }
+
+    private async loadJSON(path: string): Promise<any> {
+        return fetch(`assets/${path}`).then(response => {
+            return response.json();
+        });
+    }
+
+    public static async getImage(path: string): Promise<Sprite> {
+        const loader = Loader.getInstance();
+        return loader.loadImage(path);
+    }
+
+    private async loadImage(path: string): Promise<Sprite> {
+        return new Promise(resolve => {
+            const img = new Image();
+            img.onload = () => {
+                const sprite = new Sprite(img);
+                resolve(sprite);
+            }
+            img.src = `assets/${path}`;
+        });
+    }
+
+    public static async getSheet(path: string, positions: SpriteData[]): Promise<Sprite[]> {
+        const loader = Loader.getInstance();
+        return loader.loadSheet(path, positions);
+    }
+
+    private async loadSheet(path: string, positions: SpriteData[]): Promise<Sprite[]> {
+        return new Promise(resolve => {
+            const img = new Image();
+            img.onload = () => {
+                const sprites = [];
+                for (const position of positions) {
+                    const sprite = new Sprite(img, position.x, position.y, position.width, position.height);
+                    sprites.push(sprite);
+                }
+                resolve(sprites);
+            }
+            img.src = `assets/${path}`;
+        });
+    }
 }
---
+++ apps/character-builder/src/core/Sprite.ts
@@ -0,0 +1,44 @@
+export class Sprite {
+    private image: CanvasImageSource;
+    private sx: number;
+    private sy: number;
+    private sWidth: number;
+    private sHeight: number;
+
+    constructor(source: CanvasImageSource);
+    constructor(source: CanvasImageSource, sx: number, sy: number,
+                sWidth: number, sHeight: number);
+    constructor(source: CanvasImageSource, sx: number = 0, sy: number = 0,
+                sWidth?: number, sHeight?: number) {
+        if (!sWidth || !sHeight) {
+            sWidth = <number> source.width;
+            sHeight = <number> source.height;
+        }
+
+        this.image = source;
+        
+        this.sx = sx;
+        this.sy = sy;
+        this.sWidth = sWidth;
+        this.sHeight = sHeight;
+    }
+
+    public draw(ctx: CanvasRenderingContext2D, x: number, y: number): void;
+    public draw(ctx: CanvasRenderingContext2D, x: number, y: number, width: number, height: number): void;
+    public draw(ctx: CanvasRenderingContext2D, x: number, y: number, scale: number): void;
+    public draw(ctx: CanvasRenderingContext2D, x: number, y: number, width?: number, height?: number): void {
+        if (!width) {
+            width = this.sWidth;
+            height = this.sHeight;
+        } else if (!height) { // Scale
+            const scale = width;
+            width = this.sWidth * scale;
+            height = this.sHeight * scale;
+        }
+
+
+        const sData: [number, number, number, number] = [this.sx, this.sy, this.sWidth, this.sHeight];
+
+        ctx.drawImage(this.image, ...sData, x, y, width, height);
+    }
+}
