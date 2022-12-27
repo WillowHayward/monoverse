3cec884201087d0bc2f956e76d378750bdcabccb
Willow Hayward <WillHayCode@gmail.com>
2022-12-27 15:29:34 +1000

    build(lipwig-types): expanded lipwig types

--- libs/lipwig/types/src/lib/client.model.ts
+++ libs/lipwig/types/src/lib/client.model.ts
@@ -1,13 +1,24 @@
 /**
  * @author: WillHayCode
  */
+
 import { Message } from './common.model';
 
-type ClientMessageEvent = 'create' | 'join' | 'reconnect' | 'close' | 'administrate' | 'message'
-                        | 'lw-ping';
+//type ClientMessageEvent = 'create' | 'join' | 'reconnect' | 'close' | 'administrate' | 'message'
+//                        | 'lw-ping';
+
+enum ClientEvent {
+    CREATE = 'create',
+    JOIN = 'join',
+    RECONNECT = 'reconnect',
+    CLOSE = 'close',
+    ADMINISTRATE = 'administrate',
+    MESSAGE = 'message',
+    PING = 'lw-ping'
+}
 
 export interface ClientMessage extends Message {
-    event: ClientMessageEvent;
+    event: ClientEvent;
 };
 
 export type DataMap = {
--- libs/lipwig/types/src/lib/server.model.ts
+++ libs/lipwig/types/src/lib/server.model.ts
@@ -1,9 +1,28 @@
 /**
  * @author: WillHayCode
  */
+
 import { Message } from './common.model';
-type ServerMessageEvent = 'created' | 'joined' | 'reconnected' | 'message'
+//type ServerMessageEvent = 'created' | 'joined' | 'reconnected' | 'message'
+
+enum ServerEvent {
+    CREATED = 'created',
+    JOINED = 'joined',
+    RECONNECTED = 'reconnected',
+    MESSAGE = 'message'
+}
 
 export interface ServerMessage extends Message {
-    event: ServerMessageEvent;
+    event: ServerEvent;
 }
+
+export type LipwigOptions = {
+    port: number;
+    roomNumberLimit: number;
+    roomSizeLimit: number;
+    name: string;
+    db: string;
+};
+
+export type LipwigConfig = Partial<LipwigOptions>;
+
