89af56746e94022db062cc9ea2924730524e3d06
Willow Hayward <WillHayCode@gmail.com>
2022-12-29 14:10:10 +1000

    build(lipwig-core): removed more Nest boilerplate

--- apps/lipwig/core/src/app/app.module.ts
+++ apps/lipwig/core/src/app/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 
-import { AppController } from './app.controller';
 import { LipwigGateway } from './lipwig.gateway';
 
 @Module({
     imports: [],
-    controllers: [AppController],
+    controllers: [],
     providers: [LipwigGateway],
 })
 export class AppModule {}
