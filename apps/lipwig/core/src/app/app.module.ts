b7e5ff48a8337c48380449fdbfd21948c60da85c
Willow Hayward <WillHayCode@gmail.com>
2022-12-29 14:06:23 +1000

    build(lipwig-core): removed some boilerplate, added defaults

--- apps/lipwig/core/src/app/app.module.ts
+++ apps/lipwig/core/src/app/app.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { LipwigGateway } from './lipwig.gateway';
 
 @Module({
     imports: [],
     controllers: [AppController],
-    providers: [AppService, LipwigGateway],
+    providers: [LipwigGateway],
 })
 export class AppModule {}
