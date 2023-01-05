682d2c5f2aad1777e7da8f645f9cb5444dbad370
Willow Hayward <WillHayCode@gmail.com>
2023-01-05 20:01:06 +1000

    build(ships,jabberwocky): fixed build issues

--- apps/ships/src/engine/Player.ts
+++ apps/ships/src/engine/Player.ts
@@ -5,12 +5,12 @@ export class Player extends FlatActor {
     private camera: Camera;
     constructor(name: string, scene: Stage) {
         super(name, scene);
+        this.camera = this.createCamera();
     }
 
     public createCamera(): Camera {
         //TODO: For split screen later https://doc.babylonjs.com/how_to/how_to_use_multi-views
         let camera = new Camera(this.scene);
-        this.camera = camera;
 
         return camera;
     }
