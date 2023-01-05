a632bc4a8a5da376348e9e2e28b8e9dba35ec2e4
Willow Hayward <WillHayCode@gmail.com>
2023-01-05 20:11:57 +1000

    build(ships): removed ships project

--- apps/ships/src/engine/FlatActor.ts
+++
@@ -1,8 +0,0 @@
-import * as BABYLON from 'babylonjs';
-import { Actor, Stage } from '@whc/fps';
-export class FlatActor extends Actor {
-    constructor(name: string, scene: Stage) {
-        super(name, scene);
-        const plane = BABYLON.MeshBuilder.CreatePlane(name, {}, scene);
-    }
-}
