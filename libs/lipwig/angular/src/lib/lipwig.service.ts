7b1dce0685f787c754231cbd6bf2a8f113cd4ea4
Willow Hayward <willow@whc.fyi>
2023-06-22 17:50:09 +1000

    (lipwig): Added common angular service

---
+++ libs/lipwig/angular/src/lib/lipwig.service.ts
@@ -0,0 +1,59 @@
+import { Injectable } from '@angular/core';
+
+import { CreateOptions, JoinOptions } from '@whc/lipwig/model';
+import { Client, Host, Lipwig, LocalClient, LocalHost } from '@whc/lipwig/js';
+
+// TODO: It's an edge case, but accounting for multiple hosts/clients in a single connection could be neat
+@Injectable({
+  providedIn: 'root'
+})
+export class LipwigService {
+    private host?: Host;
+    private client?: Client;
+    public connected: boolean = false;
+
+  constructor() { }
+
+    public async create(url: string, options: CreateOptions): Promise<Host> {
+        const promise = Lipwig.create(url, options);
+        promise.then(host => {
+            this.host = host;
+            this.connected = true;
+        });
+        return promise;
+    }
+
+    public async createLocal(options: CreateOptions): Promise<LocalHost> {
+        const host = new LocalHost(options);
+        this.host = host;
+        return host;
+    }
+
+    public async join(url: string, code: string, options: JoinOptions): Promise<Client> {
+        const promise = Lipwig.join(url, code, options);
+        promise.then(client => {
+            this.client = client;
+            this.connected = true;
+        });
+        return promise;
+    }
+
+    public async joinLocal(options: JoinOptions): Promise<LocalClient> {
+        if (!this.host) {
+            throw new Error('Must create room before making local client');
+        }
+        const client = this.host.createLocalClient(options);
+        this.client = client;
+
+        return client;
+    }
+
+    public getHost(): Host | undefined {
+        return this.host;
+    }
+
+    public getClient(): Client | undefined {
+        return this.client;
+    }
+
+}
